6cd4ef4dbd429de361c9d22b633b8456b70c2a31
lee <cocoaleespring@gmail.com>
2016-06-10 10:26:29 +0800

    Initial Commit

---
+++ LSCAN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LSCAN
+//
+//  Created by Lee on 6/10/16.
+//  Copyright © 2016 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LSCAN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSCAN
+//
+//  Created by Lee on 6/10/16.
+//  Copyright © 2016 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
